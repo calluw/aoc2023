f9d6bfcc041a0880dbf8964a7aab5b50015476bd
Callum Ward <wards.callum@gmail.com>
2024-12-01 18:07:54 +0000

    Finish solution for Day 03

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -14,7 +14,7 @@ advent_of_code::solution!(3);
 
 type Coord = (isize, isize);
 
-fn coord_neighbours(coord: Coord) -> BTreeSet<Coord> {
+fn get_coord_neighbours(coord: Coord) -> BTreeSet<Coord> {
     BTreeSet::from([
         // Same line
         (coord.0, coord.1 + 1),
@@ -65,17 +65,22 @@ fn parse_schematic_line(input: &str) -> IResult<&str, Vec<(SchematicEntry, usize
     many1(parse_schematic_entry)(input)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let schematic_lines = input
+fn get_schematic(input: &str) -> Vec<Vec<(SchematicEntry, usize)>> {
+    input
         .lines()
         .map(parse_schematic_line)
         .map(|r| r.expect("parsing puzzle input succeeds"))
-        .map(|(_, schematic_line)| schematic_line);
+        .map(|(_, schematic_line)| schematic_line)
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let schematic = get_schematic(input);
 
     let mut number_coords = BTreeMap::<Coord, (u32, usize)>::new();
     let mut symbol_coords = BTreeSet::<Coord>::new();
 
-    for (i, line) in schematic_lines.enumerate() {
+    for (i, line) in schematic.into_iter().enumerate() {
         let i = i as isize;
         let mut j: isize = 0;
         for (entry, width) in line {
@@ -92,13 +97,80 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     }
 
-    for (mut coord, (num, width)) in number_coords {}
+    let mut total_touching = 0;
 
-    todo!()
+    for (coord, (num, width)) in number_coords {
+        let mut neighbour_coords = BTreeSet::new();
+        for offset in 0..width as isize {
+            let relevant_coord = (coord.0, coord.1 + offset);
+            neighbour_coords.extend(get_coord_neighbours(relevant_coord));
+        }
+
+        let touches_symbol = neighbour_coords.iter().any(|c| symbol_coords.contains(c));
+
+        if touches_symbol {
+            total_touching += num;
+        }
+    }
+
+    Some(total_touching)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    todo!()
+    let schematic = get_schematic(input);
+
+    let mut number_coords = BTreeMap::<Coord, (u32, usize)>::new();
+    let mut asterisk_coords = BTreeMap::<Coord, Vec<u32>>::new();
+
+    for (i, line) in schematic.into_iter().enumerate() {
+        let i = i as isize;
+        let mut j: isize = 0;
+        for (entry, width) in line {
+            match entry {
+                SchematicEntry::PartNumber(num) => {
+                    number_coords.insert((i, j), (num, width));
+                }
+                SchematicEntry::Symbol(ch) => {
+                    if ch == '*' {
+                        asterisk_coords.insert((i, j), Vec::new());
+                    }
+                }
+                SchematicEntry::Period => {}
+            };
+            j += width as isize;
+        }
+    }
+
+    for (coord, (num, width)) in number_coords {
+        let mut neighbour_coords = BTreeSet::new();
+        for offset in 0..width as isize {
+            let relevant_coord = (coord.0, coord.1 + offset);
+            neighbour_coords.extend(get_coord_neighbours(relevant_coord));
+        }
+
+        let mut asterisks_touched = BTreeSet::new();
+
+        for neighbour_coord in neighbour_coords {
+            if asterisk_coords.contains_key(&neighbour_coord) {
+                asterisks_touched.insert(neighbour_coord);
+            }
+        }
+
+        for asterisk_coord in asterisks_touched {
+            asterisk_coords.entry(asterisk_coord).and_modify(|nums| nums.push(num));
+        }
+    }
+
+    let mut gear_ratio_sum = 0;
+    for (_, nums) in asterisk_coords {
+        if nums.len() != 2 {
+            continue;
+        }
+
+        gear_ratio_sum += nums.iter().product::<u32>();
+    }
+
+    Some(gear_ratio_sum)
 }
 
 #[cfg(test)]
@@ -114,6 +186,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
